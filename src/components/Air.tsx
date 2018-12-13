a2e79cb50ef7f7aa214482140ef97247469fe788
nadineprigann <news@nadineprigann.de>
2018-12-13 23:22:03 +0100

    Try to fix built failure

--- src/components/Air.tsx
+++ src/components/Air.tsx
@@ -180,23 +180,3 @@ export const AirElement = styled('div')`
     position: absolute;
 `
 
-export class AirMolecules extends React.Component {
-    render() {
-      const airElements = [];
-  
-      for (let i = 0; i < 50; i++) {
-        airElements.push({
-            xPos: Math.round(((Math.random() * 665) + 1) / 35) * 35,
-            yPos: Math.round(((Math.random() * 665) + 1) / 35) * 35,
-        });
-    }
-      return(
-        <>
-            {airElements.map((airElement, i) =>
-                <AirElement key={i} style={{left: airElement.xPos, top: airElement.yPos}}/>
-            )}
-        </>
-      );
-    }
-  }
-
