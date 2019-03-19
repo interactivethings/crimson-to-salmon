1d0ebf2a1515b7da28d28ffb472c27746659ce4c
Jeremy Stucki <jeremy@interactivethings.com>
2019-03-19 16:20:44 +0100

    rm serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import { Main } from './Main';
-import './index.css';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import { Country } from './components/Countries';
-import { Station } from './components/Zurich';
+import { Main } from "./Main";
+import "./index.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Country } from "./components/Countries";
+import { Station } from "./components/Zurich";
 
 // DANGER ZONE: types may not match
-const data: Country[] = require('./data/countries.json')
-const detailMonthly: Station[] = require('./data/monthlyValues.json')
-  console.log(detailMonthly)
-  
-ReactDOM.render(
-  <Main data={data} detailMonthly={detailMonthly}/>,
-  document.getElementById('root') as HTMLElement
-);
+const data: Country[] = require("./data/countries.json");
+const detailMonthly: Station[] = require("./data/monthlyValues.json");
+console.log(detailMonthly);
 
-registerServiceWorker();
+ReactDOM.render(
+  <Main data={data} detailMonthly={detailMonthly} />,
+  document.getElementById("root") as HTMLElement
+);
